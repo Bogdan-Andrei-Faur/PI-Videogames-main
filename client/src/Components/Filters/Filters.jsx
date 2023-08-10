446f1f41afb0ebba897cabfcd2d49d5a003b4fbd
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-08-11 00:10:55 +0200

    Terminados los filtros
    Se ha creado lo necesario para filtrar los videojuegos.

--- client/src/Components/Filters/Filters.jsx
+++ client/src/Components/Filters/Filters.jsx
@@ -1,23 +1,47 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
-import { order } from "../../Redux/Actions/Actions";
+import { order, genresFilter, originFilter } from "../../Redux/Actions/Actions";
 
 export default function Filters (){
     const dispatch = useDispatch();
-    const orderState = useSelector(state => state.order)
+    const orderState = useSelector(state => state.order);
+    const genres = useSelector(state => state.genres);
+    const genresFilterState = useSelector(state => state.genresFilter);
+    const originFilterState = useSelector(state => state.originFilter);
 
     const handleOrder = event => {
         dispatch(order(event.target.value));
     }
 
+    const handleGenreFilter = event => {
+        dispatch(genresFilter(event.target.value));       
+    }
+
+    const handleOriginFilter = event => {
+        dispatch(originFilter(event.target.value));        
+    }
+
     return (
         <div>
             <select value= {orderState} onChange={handleOrder}>
-                <option value= ''>Order</option>
-                <option value= 'asc'>A - Z</option>
-                <option value= 'desc'>Z - A</option>
-                <option value= 'rating desc'>Best Rating</option>
-                <option value= 'rating asc'>Worst Rating</option>
+                <option value= "">Order</option>
+                <option value= "asc">A - Z</option>
+                <option value= "desc">Z - A</option>
+                <option value= "rating desc">Best Rating</option>
+                <option value= "rating asc">Worst Rating</option>
+            </select>
+            <select value={genresFilterState} onChange={handleGenreFilter}>
+                <option value="">Genres</option>
+                {
+                    genres?.map(genre => (
+                        <option key={genre.id} value={genre.name}>{genre.name}</option>
+                    ))
+                }
+            </select>
+            <select value={originFilterState} onChange={handleOriginFilter}>
+                <option value="">Origin</option>
+                <option value= 'created'>Created</option>
+                <option value= 'existing'>Existing</option>
             </select>
         </div>
     )
--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -3,26 +3,34 @@ import {useDispatch, useSelector} from "react-redux";
 import style from "./Home.module.css"
 import Cards from "../Cards/Cards";
 import Filters from "../Filters/Filters";
-import { getGames } from "../../Redux/Actions/Actions";
+import { getGames, getGenres } from "../../Redux/Actions/Actions";
 import { nameASC, nameDES, ratingASC, ratingDESC } from "../../Helpers/sort";
 
 export default function Home (){
     const dispatch = useDispatch();
-    const allGames = useSelector(state => state.allGames)
-    const byName = useSelector(state => state.byName)
-    const orderState = useSelector(state => state.order)
+    const allGames = useSelector(state => state.allGames);
+    const genres = useSelector(state => state.genres);
+    const byName = useSelector(state => state.byName);
+    const orderState = useSelector(state => state.order);
+    const genresFilter = useSelector(state => state.genresFilter);
+    const originFilterState = useSelector(state => state.originFilter);
     
     let games = [];
 
     byName.length ? games = [...byName] : games = [...allGames];
 
-    if(orderState === 'asc' ) games.sort(nameASC);
-    if(orderState === 'desc') games.sort(nameDES);
+    if(orderState === "asc" ) games.sort(nameASC);
+    if(orderState === "desc") games.sort(nameDES);
     if(orderState === "rating desc") games.sort(ratingDESC);
-    if(orderState === 'rating asc') games.sort(ratingASC);
+    if(orderState === "rating asc") games.sort(ratingASC);
+
+    if(genresFilter.length !== 0 && genresFilter !== "") games = games.filter(g => g.genres.includes(genresFilter));
+    if(originFilterState === 'created') games = games.filter(genre => typeof genre.id === 'string');
+    if(originFilterState === 'existing') games = games.filter(genre => typeof genre.id === 'number');   
 
     useEffect(() => {
         if(!games.length) dispatch(getGames())
+        if(!genres.length) dispatch(getGenres())
     })
 
     return (
