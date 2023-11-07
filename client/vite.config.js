82bc42566d64505baef8d82a5727238cac822bd3
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-11-07 15:04:52 +0100

    Intento 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://gamelibraryhub.onrender.com/',
   build: {
     outDir: 'dist', // Directorio de salida para los archivos compilados
     minify: true,   // Minificar el código en producción
