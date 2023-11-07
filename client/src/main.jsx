5db0e2dbfcda53024ed15126526295ebf0f1e32d
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-11-07 12:30:15 +0100

    Correccion main 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,12 +7,12 @@ import { Provider } from 'react-redux';
 import store from './Redux/Store.js';
 import './index.css'; // Importa el archivo CSS global
 
-ReactDOM.render(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
